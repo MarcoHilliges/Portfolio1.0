be859a6a03c0109f0ff81d516b118557e19b420b
MarcoHilliges <107026106+MarcoHilliges@users.noreply.github.com>
2022-07-09 23:59:19 +0200

    backup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DataProtectionComponent } from './data-protection/data-protection.component';
+import { ImprintComponent } from './imprint/imprint.component';
+import { PortfolioContentComponent } from './portfolio-content/portfolio-content.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: PortfolioContentComponent},
+  {path: 'imprint', component: ImprintComponent},
+  {path: 'data-protection', component: DataProtectionComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { DataProtectionComponent } from './data-protection/data-protection.component';
+import { FooterComponent } from './footer/footer.component';
+import { ImprintComponent } from './imprint/imprint.component';
+import { MenuComponent } from './menu/menu.component';
+import { PortfolioContentComponent } from './portfolio-content/portfolio-content.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { SkillsComponent } from './skills/skills.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    ContactComponent,
+    DataProtectionComponent,
+    FooterComponent,
+    ImprintComponent,
+    MenuComponent,
+    PortfolioContentComponent,
+    ProjectsComponent,
+    SkillsComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
